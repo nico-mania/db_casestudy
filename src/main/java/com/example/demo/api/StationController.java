be7be80ee35bdaa135758d0e841e8af6477cefde
nico-mania <55563178+nico-mania@users.noreply.github.com>
2023-07-26 05:40:00 +0200

    StationController changed to reach new endpoint

--- src/main/java/com/example/demo/api/StationController.java
+++ src/main/java/com/example/demo/api/StationController.java
@@ -2,6 +2,8 @@ package com.example.demo.api;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.stream.XMLInputFactory;
@@ -9,18 +11,57 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/station")
 public class StationController {
 
-    @GetMapping("/station")
-    Station station() throws XMLStreamException, IOException {
-        InputStream xmlResource = StationController.class.getClassLoader().getResourceAsStream("xml/0Test.xml");
-        XMLInputFactory xmlInputFactory = javax.xml.stream.XMLInputFactory.newFactory();
-        XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(xmlResource);
+    @GetMapping(value = "{shortcode}/train/{trainNumber}/waggon/{number}")
+    public List<String> getTrackSections(@PathVariable String shortcode, @PathVariable int trainNumber, @PathVariable int number) {
+        List<String> trackSections = new ArrayList<>();
 
-        XmlMapper mapper = new XmlMapper();
+        try {
+            Station station = getStationData(); // Implement the method to get Station object from XML
 
-        return mapper.readValue(xmlStreamReader, Station.class);
+            assert station != null;
+            for (Track track : station.getTracks()) {
+                for (Train train : track.getTrains()) {
+                    if (train.getTrainNumbers().contains(trainNumber)) {
+                        for (Waggon waggon : train.getWaggons()) {
+                            if (waggon.getPosition() == number) {
+                                trackSections.addAll(waggon.getSections());
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // Handle any exceptions or return an error response if necessary.
+            e.printStackTrace();
+        }
+
+        return trackSections;
+    }
+
+    // Implement the method to read XML data and convert it into a Station object.
+    private Station getStationData() {
+        try {
+            // Load the XML file as an InputStream
+            InputStream xmlResource = StationController.class.getClassLoader().getResourceAsStream("xml/0Test.xml");
+
+            // Create an XmlMapper instance
+            XmlMapper xmlMapper = new XmlMapper();
+
+            // Deserialize the XML data into a Station object
+
+            return xmlMapper.readValue(xmlResource, Station.class);
+        } catch (Exception e) {
+            // Handle any exceptions that might occur during XML parsing
+            e.printStackTrace();
+        }
+
+        return null; // Return null if the XML data couldn't be parsed or encountered an error.
     }
 }
